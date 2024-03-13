3a32802b60b2e389ec2a0cff69924104ac46ba0e
Santiago Alzate C <salzate361@misena.edu.co>
2024-03-13 12:49:45 -0500

    Se extiende plano

--- Assets/OldAssets/Scripts/GroundMover.cs
+++ Assets/OldAssets/Scripts/GroundMover.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 
 public class GroundMover : MonoBehaviour
 {
+    
     public float velocidad = 5f;
+    private float tiempoParaIncrementar = 8f;
+    private float tiempoTranscurrido = 0f;
 
     void Update()
     {
-        // Mover el suelo hacia atrás
-        transform.Translate(-velocidad * Time.deltaTime,0f,0f);
+        // Mover el suelo hacia atrÃ¡s
+        transform.Translate(-velocidad * Time.deltaTime, 0f, 0f);
 
-        // Revisar si el suelo ha salido del campo de visión y resetearlo
-        //if (transform.position.z < -50f)
-        //{
-        //    // Ajusta el valor de -50f según la longitud de tu pista
-        //    transform.Translate(Vector2.forward * 100f);
-        //}
+        // Incrementar el tiempo transcurrido
+        tiempoTranscurrido += Time.deltaTime;
+
+        // Verificar si han pasado 8 segundos
+        if (tiempoTranscurrido >= tiempoParaIncrementar)
+        {
+            // Aumentar la velocidad en un 10%
+            velocidad *= 1.1f;
+            // Resetear el contador de tiempo
+            tiempoTranscurrido = 0f;
+        }        //}
     }
 }
 
