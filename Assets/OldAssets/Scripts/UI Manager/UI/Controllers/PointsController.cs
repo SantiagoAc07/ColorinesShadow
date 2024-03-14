134f0b83d76e23c5f313cf255b4f081f585fc503
Santiago Alzate C <salzate361@misena.edu.co>
2024-03-14 17:52:47 -0500

    Casi terminadoooo

--- Assets/OldAssets/Scripts/UI Manager/UI/Controllers/PointsController.cs
+++ Assets/OldAssets/Scripts/UI Manager/UI/Controllers/PointsController.cs
@@ -4,18 +4,19 @@ using TMPro;
 
 public class PointsController : MonoBehaviour
 {
-    public List<NumberValueView> numberValueViews = new List<NumberValueView>();
-    private TMP_Text tex;
+    //public List<NumberValueView> numberValueViews = new List<NumberValueView>();
+    public TMP_Text tex;
 
     public void UpdatePoints(int points)
     {
-        for (int i = numberValueViews.Count - 1; i >= 0; i--)
-        {
-            int currentNumber = points % 10;
-            points /= 10;
+//        for (int i = numberValueViews.Count - 1; i >= 0; i--)
+//        {
+//            int currentNumber = points % 10;
+//            points /= 10;
+//
+//            numberValueViews[i].SetNumber(currentNumber);
+//        }
 
-            numberValueViews[i].SetNumber(currentNumber);
-            tex.text = points.ToString("F0");
-        }
+        tex.text = points.ToString();
     }
 }
